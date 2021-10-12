340f41a38ff305742c3599548c9718b86e063cc4
iminchausti <80271753+iminchausti@users.noreply.github.com>
2021-10-12 14:59:11 -0400

    Add files via upload

---
+++ WebPlatformSite/src/components/announcement.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Alert } from "react-bootstrap";
+
+class Announcement extends Component {
+  render() {
+    <h1>Test</h1>;
+  }
+}
+
+export default Announcement;
